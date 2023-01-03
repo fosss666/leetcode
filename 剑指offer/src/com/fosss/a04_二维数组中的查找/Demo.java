0749c15f7c9bcbd9ef29b502e01ec63685e27cf7
fo <1745179058@qq.com>
2023-01-03 16:00:01 +0800

    04_二维数组中的查找_Z字形查找

--- 剑指offer/src/com/fosss/a04_二维数组中的查找/Demo.java
+++ 剑指offer/src/com/fosss/a04_二维数组中的查找/Demo.java
@@ -20,19 +20,54 @@ package com.fosss.a04_二维数组中的查找;
 public class Demo {
     public static void main(String[] args) {
         int[][] matrix = {
-                {1, 4, 7, 11, 15},
-                {2, 5, 8, 12, 19},
-                {3, 6, 9, 16, 22},
-                {10, 13, 14, 17, 24},
-                {18, 21, 23, 26, 30}
+                //{1, 4, 7, 11, 15},
+                //{2, 5, 8, 12, 19},
+                //{3, 6, 9, 16, 22},
+                //{10, 13, 14, 17, 24},
+                //{18, 21, 23, 26, 30}
 
                 //{}
         };
         Demo demo = new Demo();
         int target = 10;
         //boolean numberIn2DArray = demo.findNumberIn2DArray(matrix, target);
-        boolean numberIn2DArray = demo.findNumberIn2DArray2(matrix, target);
+        //boolean numberIn2DArray = demo.findNumberIn2DArray2(matrix, target);
+        boolean numberIn2DArray = demo.findNumberIn2DArray3(matrix, target);
         System.out.println("numberIn2DArray = " + numberIn2DArray);
+    }
+
+    /**
+     * Z 字形查找
+     * 我们可以从矩阵matrix 的右上角 (0, n-1)(0,n−1) 进行搜索。在每一步的搜索过程中，如果我们位于位置(x,y)，
+     * 那么我们希望在以matrix 的左下角为左下角、以 (x, y)为右上角的矩阵中进行搜索，即行的范围为[x,m−1]，列的范围为 [0, y]
+     * 如果 matrix[x, y] ==target，说明搜索完成；
+     * 如果 matrix[x, y] >target，由于每一列的元素都是升序排列的，那么在当前的搜索矩阵中，所有位于第y列的元素都是严格大于target的，
+     * 因此我们可以将它们全部忽略，即将y减少1；
+     * 如果 matrix[x, y] <target，由于每一行的元素都是升序排列的，那么在当前的搜索矩阵中，所有位于第x行的元素都是严格小于target的，
+     * 因此我们可以将它们全部忽略，即将x增加1。
+     * 在搜索的过程中，如果我们超出了矩阵的边界，那么说明矩阵中不存在target。
+     */
+    public boolean findNumberIn2DArray3(int[][] matrix, int target) {
+        int m = matrix.length;
+        if (m == 0) {
+            //空数组
+            return false;
+        }
+        int n = matrix[0].length;
+        int x = 0;
+        int y = n - 1;
+        while (x < m && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (matrix[x][y] < target) {
+                //向下找
+                x++;
+            } else if (matrix[x][y] > target) {
+                //向左找
+                y--;
+            }
+        }
+        return false;
     }
 
     /**
