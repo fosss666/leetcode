4ca2278a538b2c10dbd78f0e2ee5afb7e35ae145
fo <1745179058@qq.com>
2023-01-17 13:51:39 +0800

    24_反转链表_自解—使用栈

---
+++ 剑指offer/src/com/fosss/a24_反转链表/ListNode.java
@@ -0,0 +1,7 @@
+package com.fosss.a24_反转链表;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
---
+++ 剑指offer/src/com/fosss/a24_反转链表/Solution.java
@@ -0,0 +1,72 @@
+package com.fosss.a24_反转链表;
+
+import java.util.Stack;
+
+/**
+ * @author fosss
+ * @date 2023/1/17
+ * @description： 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+ * 例：输入: 1->2->3->4->5->NULL  输出: 5->4->3->2->1->NULL
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        ListNode node = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        node.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        Solution solution = new Solution();
+        ListNode newHead = solution.reverseList(node);
+        while (newHead != null) {
+            System.out.print(newHead.val + " ");
+            newHead = newHead.next;
+        }
+
+    }
+
+    /**
+     * 自解，使用栈
+     */
+    public ListNode reverseList(ListNode head) {
+        if(head == null){
+            return head;
+        }
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        ListNode newHead = new ListNode(stack.pop());
+        ListNode t = newHead;
+        while (!stack.isEmpty()) {
+            ListNode temp = new ListNode(stack.pop());
+            t.next = temp;
+            t = t.next;
+        }
+        return newHead;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
