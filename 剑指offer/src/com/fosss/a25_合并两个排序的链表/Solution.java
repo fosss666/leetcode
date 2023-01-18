c8f2dbc83c6f61f7455ac43bc9f82b1981e79450
fo <1745179058@qq.com>
2023-01-18 15:52:00 +0800

    25_合并两个排序的链表_自解，逐个插入，优化版，从上一个插入的地方作为链表起始地方进行遍历插入，速度并没有显著提高，舍弃舍弃

--- 剑指offer/src/com/fosss/a25_合并两个排序的链表/Solution.java
+++ 剑指offer/src/com/fosss/a25_合并两个排序的链表/Solution.java
@@ -9,24 +9,72 @@ package com.fosss.a25_合并两个排序的链表;
 public class Solution {
 
     public static void main(String[] args) {
-        ListNode node1 = new ListNode(-9);
-        ListNode node2 = new ListNode(3);
-        //ListNode node3 = new ListNode(4);
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(4);
         node1.next = node2;
-        //node2.next = node3;
+        node2.next = node3;
 
-        ListNode node11 = new ListNode(5);
-        ListNode node22 = new ListNode(7);
-        //ListNode node33 = new ListNode(4);
+        ListNode node11 = new ListNode(1);
+        ListNode node22 = new ListNode(3);
+        ListNode node33 = new ListNode(4);
         node11.next = node22;
-        //node22.next = node33;
+        node22.next = node33;
 
         Solution solution = new Solution();
-        ListNode newHead = solution.mergeTwoLists2(node1, node11);
+        ListNode newHead = solution.mergeTwoLists3(node1, node11);
         while (newHead != null) {
             System.out.print(newHead.val + " ");
             newHead = newHead.next;
         }
+    }
+
+    /**
+     * 自解，逐个插入，优化版，从上一个插入的地方作为链表起始地方进行遍历插入，速度并没有显著提高，舍弃舍弃
+     */
+    public ListNode mergeTwoLists3(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+        ListNode newHead=l1;
+        //遍历一个链表，将其结点的数值插入到另一个链表中
+        while (l2 != null) {
+            newHead = insertVal2(newHead, l2.val);
+            l2 = l2.next;
+        }
+        return newHead;
+    }
+
+    //插入结点的位置
+    public static ListNode insertNode = null;
+    //向链表中插入数值
+    private ListNode insertVal2(ListNode head, int val) {
+        if (head.val >= val) {
+            ListNode listNode = new ListNode(val);
+            listNode.next = head;
+            return listNode;
+        }
+        ListNode temp = null;
+        if (insertNode != null) {
+            temp = insertNode;
+        } else {
+            temp = head;
+        }
+        while (temp.next != null) {
+            if (temp.next.val >= val) {
+                ListNode listNode = new ListNode(val);
+                listNode.next = temp.next;
+                temp.next = listNode;
+                //插入完毕，返回插入结点的位置
+                return head;
+            }
+            temp = temp.next;
+        }
+        //走到这一步，说明要插入的值比链表中所有节点的值都要大
+        temp.next = new ListNode(val);
+        return head;
     }
 
     /**
