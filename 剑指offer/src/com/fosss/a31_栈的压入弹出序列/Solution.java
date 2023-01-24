70c534e65418edc998f0889faea12b1927343a86
fo <1745179058@qq.com>
2023-01-24 14:51:53 +0800

    31_栈的压入弹出序列_辅助栈

---
+++ 剑指offer/src/com/fosss/a31_栈的压入弹出序列/Solution.java
@@ -0,0 +1,62 @@
+package com.fosss.a31_栈的压入弹出序列;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * @author fosss
+ * @date 2023/1/24
+ * @description： 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否为该栈的弹出顺序。假设压入栈的所有数字均不相等。例如，序列 {1,2,3,4,5}
+ * 是某栈的压栈序列，序列 {4,5,3,2,1} 是该压栈序列对应的一个弹出序列，但 {4,3,5,1,2} 就不可能是该压栈序列的弹出序列
+ * 示例： 输入：pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+ * 输出：true
+ * 解释：我们可以按以下顺序执行：
+ * push(1), push(2), push(3), push(4), pop() -> 4,
+ * push(5), pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+ */
+public class Solution {
+    public static void main(String[] args) {
+        int[] pushed = {1, 0, 2};
+        int[] popped = {2, 1, 0};
+        Solution solution = new Solution();
+        boolean result = solution.validateStackSequences(pushed, popped);
+        System.out.println("result = " + result);
+    }
+
+    /**
+     * 辅助栈
+     */
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for (int p : pushed) {
+            stack.push(p);
+            while (!stack.isEmpty() && stack.peek() == popped[i]) {
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
