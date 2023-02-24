f57b9bdb7a427d87633452c80bf7743010e2a0d0
fo <1745179058@qq.com>
2023-02-24 12:17:45 +0800

    56-1-数组中数字出现的次数-自解，先排序再判断 17%  18%

--- 剑指offer/src/com/fosss/a56_1_数组中数字出现的次数/Solution.java
+++ 剑指offer/src/com/fosss/a56_1_数组中数字出现的次数/Solution.java
@@ -15,8 +15,40 @@ public class Solution {
     public static void main(String[] args) {
         int[] nums = {4, 1, 4, 6};
         Solution solution = new Solution();
-        int[] res = solution.singleNumbers(nums);
+        int[] res = solution.singleNumbers2(nums);
         System.out.println("res = " + Arrays.toString(res));
+    }
+
+    /**
+     * 自解，先排序再判断 17%  18%
+     */
+    public int[] singleNumbers2(int[] nums) {
+        //先排序
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<>();
+        int i = 0, j = 1;
+        while (list.size() < 2 && i < nums.length && j < nums.length) {
+            if (nums[i] != nums[j]) {
+                //相邻两个数不同的话，说明前一个数时符合要求的
+                list.add(nums[i]);
+                //判断下一个数
+                i++;
+                j++;
+            } else {
+                //这两个数是不符合要求的，跳过这两个数
+                i += 2;
+                j = i + 1;
+            }
+        }
+        //如果是这种情况 【1，4，4，6】，需要将最后一个数添加到集合中
+        if (list.size() < 2) {
+            list.add(nums[nums.length - 1]);
+        }
+        int[] res = new int[2];
+        for (int i1 = 0; i1 < list.size(); i1++) {
+            res[i1] = list.get(i1);
+        }
+        return res;
     }
 
     /**
