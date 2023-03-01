5f5b31a127acf46e9ec618dd44d5e77eef979de1
fo <1745179058@qq.com>
2023-03-01 11:16:25 +0800

    58-2-左旋转字符串-自解，不利用StringBuilder拼接字符串  5%  5%

--- 剑指offer/src/com/fosss/a58_2_左旋转字符串/Solution.java
+++ 剑指offer/src/com/fosss/a58_2_左旋转字符串/Solution.java
@@ -8,9 +8,27 @@ package com.fosss.a58_2_左旋转字符串;
  * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，
  * 该函数将返回左旋转两位得到的结果"cdefgab"
  * 限制：1 <= k < s.length <= 10000
- *
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String res = solution.reverseLeftWords2("abcdefg", 2);
+        System.out.println("res = " + res);
+    }
+
+
+    /**
+     * 自解，不利用StringBuilder拼接字符串  5%  5%
+     */
+    public String reverseLeftWords2(String s, int n) {
+        for (int i = 0; i < n; i++) {
+            s += s.charAt(0);
+            s = s.substring(1);
+        }
+        return s;
+    }
 
     /**
      * 自解，拼接字符串   6%  48%
