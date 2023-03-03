6c9c086668a108f6b6e122cfc3a4b065a76f6818
fo <1745179058@qq.com>
2023-03-03 10:21:41 +0800

    62-圆圈中最后剩下的数字-自解，List集合，超时-_-!

---
+++ 剑指offer/src/com/fosss/a62_圆圈中最后剩下的数字/Solution.java
@@ -0,0 +1,74 @@
+package com.fosss.a62_圆圈中最后剩下的数字;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: fosss
+ * Date: 2023/3/3
+ * Time: 9:59
+ * Description: 0,1,···,n-1这n个数字排成一个圆圈，从数字0开始，每次从这个圆圈里删除第m个数字（删除后从下一个数字开始计数）。求出这个圆圈里剩
+ * 下的最后一个数字。
+ * 例如，0、1、2、3、4这5个数字组成一个圆圈，从数字0开始每次删除第3个数字，则删除的前4个数字依次是2、0、4、1，因此最后剩下的数字是3
+ * 限制：1 <= n <= 10^5  1 <= m <= 10^6
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int res = solution.lastRemaining(5, 2);
+        System.out.println("res = " + res);
+    }
+
+
+    /**
+     * 自解，List集合，超时-_-!
+     */
+    public int lastRemaining(int n, int m) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int i = 1;
+        int index = 0;
+        while (list.size() > 1) {
+            if (index == list.size()) {
+                index = 0;
+            }
+            if (i % m == 0) {
+                //如果删除了元素，则下标不用增加，直接指向了下一个值
+                list.remove(index);
+                i = 0;
+            } else {
+                index++;
+            }
+            i++;
+
+        }
+        return list.get(0);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
