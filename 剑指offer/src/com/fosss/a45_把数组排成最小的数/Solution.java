17fcfa2f9e9b7a41c3fd58ccd911d4d437ca7cde
fo <1745179058@qq.com>
2023-03-12 13:29:39 +0800

    45-把数组排成最小的数-自定义排序，快速排序  99%  43%

---
+++ 剑指offer/src/com/fosss/a45_把数组排成最小的数/Solution.java
@@ -0,0 +1,92 @@
+package com.fosss.a45_把数组排成最小的数;
+
+/**
+ * @author: fosss
+ * Date: 2023/3/12
+ * Time: 12:15
+ * Description:输入一个非负整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个
+ * 示例：输入: [10,2]  输出: "102"
+ * 限制：0 < nums.length <= 100
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {3, 10, 2};
+        String res = solution.minNumber(nums);
+        System.out.println("res = " + res);
+    }
+
+    /**
+     * 自定义排序，快速排序  99%  43%
+     */
+    public String minNumber(int[] nums) {
+        String[] strings = new String[nums.length];
+        //转为字符串数组
+        for (int i = 0; i < nums.length; i++) {
+            strings[i] = String.valueOf(nums[i]);
+        }
+        //自定义快排
+        quickSort(strings, 0, strings.length - 1);
+        StringBuilder sb = new StringBuilder();
+        for (String string : strings) {
+            sb.append(string);
+        }
+        return sb.toString();
+
+    }
+
+    private void quickSort(String[] strings, int left, int right) {
+        if (left < right) {
+            String mid = strings[left];
+            int l = left, r = right;
+            while (l < r) {
+                //x+y>y+x时，交换
+                while (l < r && (mid + strings[r]).compareTo(strings[r] + mid) <= 0) {
+                    r--;
+                }
+                if (l < r) {
+                    strings[l] = strings[r];
+                    l++;
+                }
+                while (l < r && (mid + strings[l]).compareTo(strings[l] + mid) > 0) {
+                    l++;
+                }
+                if (l < r) {
+                    strings[r] = strings[l];
+                    r--;
+                }
+            }
+            //循环退出时 l==r
+            strings[l] = mid;
+
+            quickSort(strings, left, r - 1);
+            quickSort(strings, l + 1, right);
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
