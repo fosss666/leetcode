748f947c67fc7460cf9954c87208016a71cd6a26
fo <1745179058@qq.com>
2023-03-17 10:02:17 +0800

    RELEARN-a05_替换空格

--- 剑指offer/src/com/fosss/a05_替换空格/Demo.java
+++ 剑指offer/src/com/fosss/a05_替换空格/Demo.java
@@ -1,9 +1,5 @@
 package com.fosss.a05_替换空格;
 
-import com.sun.org.apache.xml.internal.serializer.ElemDesc;
-
-import java.util.Arrays;
-
 /**
  * @author fosss
  * date 2023/1/3
@@ -11,7 +7,12 @@ import java.util.Arrays;
  * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
  * 例：输入：s = "We are happy."
  * 输出："We%20are%20happy."
- * 限制：0 <= s 的长度 <= 1000
+ * 限制：0 <= s 的长度 <= 1000、
+ * <p>
+ * 思路：
+ * 1.用索引index遍历，遇到空格则换成%20后，index+=3,可以通过新建字符数组字符数组（3倍字符串长度，因为1个字符换成3个字符，最多长度增长3倍），
+ * 也可借助StringBuilder进行拼接
+ *
  */
 public class Demo {
     public static void main(String[] args) {
@@ -26,13 +27,13 @@ public class Demo {
     /**
      * StringBuilder
      */
-    public String replaceSpace3(String s){
+    public String replaceSpace3(String s) {
 
         StringBuilder sb = new StringBuilder();
         for (char c : s.toCharArray()) {
-            if(c==' '){
+            if (c == ' ') {
                 sb.append("%20");
-            }else {
+            } else {
                 sb.append(c);
             }
         }
