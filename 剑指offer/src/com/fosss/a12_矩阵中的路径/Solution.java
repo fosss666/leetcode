76602774b98217f0024bd98bf3d54cee8ce00150
fo <1745179058@qq.com>
2023-03-19 13:20:26 +0800

    RELEARN-a12_矩阵中的路径

--- 剑指offer/src/com/fosss/a12_矩阵中的路径/Solution.java
+++ 剑指offer/src/com/fosss/a12_矩阵中的路径/Solution.java
@@ -13,6 +13,9 @@ package com.fosss.a12_矩阵中的路径;
  * 1 <= m, n <= 6
  * 1 <= word.length <= 15
  * board 和 word 仅由大小写英文字母组成
+ *
+ * 思路：遍历二维字符网格的每个字符，对符合word第一个字符的地方进行寻找
+ *
  */
 public class Solution {
     public static void main(String[] args) {
@@ -49,9 +52,19 @@ public class Solution {
     }
 
 
+    /**
+     * 判断从（i,j）开始的单词是否符合条件
+     * @param i 第几行
+     * @param j 第几列
+     * @param board 二维字符网格
+     * @param word 要查找的单词
+     * @param k 已查找的符合条件的字母在单词中对应的下标，初始为0
+     * @param isVisited 二维网格的每个位置是否已访问
+     * @return 返回是否找到
+     */
     private boolean check(int i, int j, char[][] board, String word, int k, boolean[][] isVisited) {
         if (board[i][j] != word.charAt(k)) {
-            //说明第一个字符不匹配，直接返回false
+            //说明字符不匹配，直接返回false
             return false;
         } else if (k == word.length() - 1) {
             //说明找到了
@@ -62,18 +75,19 @@ public class Solution {
                 {-1, 0},//上
                 {0, 1},//右
                 {1, 0},//下
-                {0, -1},//左
+                {0, -1}//左
         };
 
         //到此处(i,j)的点为word中k处的字符，标记为已访问
         isVisited[i][j] = true;
+        //记录是否找到单词
         boolean result = false;
         //下面对上下左右进行判断
         for (int x = 0; x < moving.length; x++) {
             int newI = i + moving[x][0];
             int newJ = j + moving[x][1];
             if (newI >= 0 && newI < board.length && newJ >= 0 && newJ < board[i].length) {
-                if (board[newI][newJ] == word.charAt(k + 1) && !isVisited[newI][newJ]) {
+                if (!isVisited[newI][newJ]) {
                     boolean check = check(newI, newJ, board, word, k + 1, isVisited);
                     if (check) {
                         //找到了
@@ -83,7 +97,7 @@ public class Solution {
                 }
             }
         }
-        //如果程序进行到这一行，说明（i,j）处不符合条件，则会进行判断其他点，需要将（i,j）点设置为未访问!!!
+        //如果程序进行到这一行，可能找到，也可能没有找到，没有找到的话为了避免后续寻找过程中会用到（i,j）这个字符，所以需要将此处设为未访问，
         isVisited[i][j] = false;
         return result;
     }
