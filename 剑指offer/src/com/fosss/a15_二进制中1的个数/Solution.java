6f1414a9726c6173e575a987a782a21fbb220a14
fo <1745179058@qq.com>
2023-03-21 10:48:55 +0800

    RELEARN-a15_二进制中1的个数

--- 剑指offer/src/com/fosss/a15_二进制中1的个数/Solution.java
+++ 剑指offer/src/com/fosss/a15_二进制中1的个数/Solution.java
@@ -1,7 +1,5 @@
 package com.fosss.a15_二进制中1的个数;
 
-import java.util.Scanner;
-
 /**
  * @author fosss
  * date 2023/1/10
@@ -9,6 +7,9 @@ import java.util.Scanner;
  * 编写一个函数，输入是一个无符号整数（以二进制串的形式），返回其二进制表达式中数字位数为 '1' 的个数（也被称为 汉明重量).）
  * 例：输入：n = 11 (控制台输入 00000000000000000000000000001011)
  * 输出：3
+ * <p>
+ * 思考：
+ * 使用运算符，用&1来记录最右的一位是不是1，用无符号右移>>>来移动到下一位
  */
 public class Solution {
     public static void main(String[] args) {
@@ -16,10 +17,11 @@ public class Solution {
         int result = solution.hammingWeight2(11);
         System.out.println("result = " + result);
     }
+
     /**
      * 调用api
      */
-    public int hammingWeight2(int n){
+    public int hammingWeight2(int n) {
         return Integer.bitCount(n);
     }
 
