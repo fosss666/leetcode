c347556a9aacdd1ddd507bcac22d8e98ba5d8752
fo <1745179058@qq.com>
2023-03-29 20:59:47 +0800

    RELEARN-a40_最小的k个数

--- 剑指offer/src/com/fosss/a40_最小的k个数/Solution.java
+++ 剑指offer/src/com/fosss/a40_最小的k个数/Solution.java
@@ -1,7 +1,6 @@
 package com.fosss.a40_最小的k个数;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 /**
  * @author fosss
@@ -9,6 +8,9 @@ import java.util.stream.Collectors;
  * @description： 输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4
  * 例：输入：arr = [3,2,1], k = 2  输出：[1,2] 或者 [2,1]
  * 限制：0 <= k <= arr.length <= 10000    0 <= arr[i] <= 10000
+ *
+ * 思路：
+ * 从小到大排序后返回前k个数，所以选择合适的排序算法即可
  */
 public class Solution {
 
