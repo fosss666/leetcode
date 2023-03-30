c5213cb6f6f0a273fd1354c2de65bac927f7b298
fo <1745179058@qq.com>
2023-03-30 22:02:26 +0800

    RELEARN-a42_连续子数组的最大和

--- 剑指offer/src/com/fosss/a42_连续子数组的最大和/Solution.java
+++ 剑指offer/src/com/fosss/a42_连续子数组的最大和/Solution.java
@@ -6,18 +6,58 @@ package com.fosss.a42_连续子数组的最大和;
  * @description： 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值  要求时间复杂度为O(n)
  * 例：输入: nums = [-2,1,-3,4,-1,2,1,-5,4] 输出: 6  解释: 连续子数组 [4,-1,2,1] 的和最大，为 6
  * 提示：1 <= arr.length <= 10^5   -100 <= arr[i] <= 100
+ * <p>
+ * 思路：
+ * 动态规划，dp[i]表示遍历到第下标为i的元素时，前边“相邻”连续子数组和的最大值，不代表所有子数组和的最大值
  */
 public class Solution {
 
     public static void main(String[] args) {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         Solution solution = new Solution();
-        int res = solution.maxSubArray(nums);
+        int res = solution.maxSubArray2(nums);
         System.out.println("res = " + res);
     }
 
     /**
-     * 动态规划
+     * 动态规划-优化版
+     */
+    public int maxSubArray3(int[] nums) {
+        int max = nums[0];//记录最大值
+        int pre = 0;//记录dp[i-1]
+
+        for (int num : nums) {
+            int cur = pre > 0 ? num + pre : num;
+            max = Math.max(max, cur);
+            pre = cur;//更新pre
+        }
+        return max;
+    }
+
+    /**
+     * 动态规划-dp数组版
+     */
+    public int maxSubArray2(int[] nums) {
+
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = nums[0];
+        //定义一个变量记录最大值
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int cur = dp[i - 1] > 0 ? dp[i - 1] + nums[i] : nums[i];
+            max = Math.max(max, cur);
+            dp[i] = cur;
+        }
+        return max;
+    }
+
+    /**
+     * 动态规划-以原nums数组作为dp数组，不推荐
      */
     public int maxSubArray(int[] nums) {
         int res = nums[0];
