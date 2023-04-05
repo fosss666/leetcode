8ff4cf56589230f95bde4e129b08083c3c262b74
fo <1745179058@qq.com>
2023-04-05 13:35:36 +0800

    RELEARN-a54_二叉搜索树的第k大节点

--- 剑指offer/src/com/fosss/a54_二叉搜索树的第k大节点/Solution.java
+++ 剑指offer/src/com/fosss/a54_二叉搜索树的第k大节点/Solution.java
@@ -1,8 +1,7 @@
 package com.fosss.a54_二叉搜索树的第k大节点;
 
-import sun.reflect.generics.tree.Tree;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author fosss
@@ -10,6 +9,11 @@ import java.util.*;
  * @description： 给定一棵二叉搜索树，请找出其中第 k 大的节点的值
  * 例：输入: root = [3,1,4,null,2], k = 1  输出：4
  * 限制：1 ≤ k ≤ 二叉搜索树元素个数
+ * <p>
+ * 思路：
+ * 首先要明确二叉搜索树的特点，对二叉搜索树进行中序遍历就可得到一个升序的数组，第k大就是下标为 (nums.length-k) 处的元素。中序遍历是按照左节点->根节点->右
+ * 节点的顺序进行遍历的，同理，按照右节点->根节点->左节点的顺序进行遍历可以得到一个降序排列的数组，第k大就是下标为 (k-1) 处的元素。依照这两种方式可以都可以
+ * 进行求解。在用方式2时，可以不使用集合进行存储。
  */
 public class Solution {
 
@@ -29,24 +33,25 @@ public class Solution {
 
     /**
      * 中序遍历，优化  100%
-     * 二叉搜索树按照右结点->根节点->左结点的顺序遍历就是从大到小遍历
+     * 二叉搜索树按照右结点->根节点->左结点的顺序遍历就是从大到小遍历，不利用List进行存储
      */
     public int kthLargest2(TreeNode root, int k) {
         if (root == null) {
             return -1;
         }
-        middle2(root,k);
+        middle2(root, k);
         return res;
     }
 
-    int num=0,res=-1;
+    int num = 1, res = -1;
+
     private void middle2(TreeNode root, int k) {
         if (root.right != null) {
             middle2(root.right, k);
         }
-        //++num 把这个条件放到括号里是关键
-        if(++num==k){
-            res=root.val;
+
+        if (num++ == k) {
+            res = root.val;
             return;
         }
 
@@ -66,7 +71,7 @@ public class Solution {
         List<Integer> list = new ArrayList<>();
         middle(root, list);
         //第k大，就是正数第（n-k）个数
-        return list.get(list.size()-k);
+        return list.get(list.size() - k);
 
         /*for (int i = 0; i < list.size(); i++) {
             if (list.size() - k == i) {
