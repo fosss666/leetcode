dfb346b02b1d04dd13cf646ffe077ad12df55051
fo <1745179058@qq.com>
2023-04-17 21:54:21 +0800

    RELEARN-a66_构建乘积数组

--- 剑指offer/src/com/fosss/a66_构建乘积数组/Solution.java
+++ 剑指offer/src/com/fosss/a66_构建乘积数组/Solution.java
@@ -10,6 +10,10 @@ import java.util.Arrays;
  * B[i]=A[0]×A[1]×…×A[i-1]×A[i+1]×…×A[n-1]。不能使用除法
  * 示例：输入: [1,2,3,4,5]  输出: [120,60,40,30,24]
  * 提示：所有元素乘积之和不会溢出 32 位整数； a.length <= 100000
+ * <p>
+ * 思路：
+ * 不能用除法，也就不能用所有数的乘积除以当前值的方法
+ * 解法：动态规划，分别求i左侧的乘积和i右侧的乘积，最后再乘起来。
  */
 public class Solution {
 
@@ -27,21 +31,26 @@ public class Solution {
         if (a == null || a.length == 0) {
             return new int[0];
         }
+        //返回结果组合
         int[] b = new int[a.length];
+        //左侧乘积
         int[] left = new int[a.length];
+        //右侧乘积
         int[] right = new int[a.length];
 
-        //初始化 1
+        //初始化第一个数左侧乘积为1，最后一个数右侧乘积为1
         left[0] = 1;
         right[a.length - 1] = 1;
 
-
+        //求i左侧的数乘积
         for (int i = 1; i < a.length; i++) {
-            //求i左侧乘积
             left[i] = left[i - 1] * a[i - 1];
-            //求i右侧乘积
-            right[a.length - 1 - i] = right[a.length - i] * a[a.length - i];
         }
+        //求i右侧的数乘积
+        for (int i = a.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * a[i + 1];
+        }
+
         //求总乘积
         for (int i = 0; i < a.length; i++) {
             b[i] = left[i] * right[i];
