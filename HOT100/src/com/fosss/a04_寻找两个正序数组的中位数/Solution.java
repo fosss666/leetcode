3777d0df0b65039eccd5a72565d690b8eacee245
fo <1745179058@qq.com>
2023-05-01 19:13:17 +0800

    Hot100-04-寻找两个正序数组的中位数-双指针，时间复杂度为O(m+n)

--- HOT100/src/com/fosss/a04_寻找两个正序数组的中位数/Solution.java
+++ HOT100/src/com/fosss/a04_寻找两个正序数组的中位数/Solution.java
@@ -26,6 +26,33 @@ public class Solution {
     }
 
     /**
+     * 双指针，时间复杂度为O(m+n)
+     * 无论总数是奇数还是偶数，都需要遍历到第n/2+1个数，区别在于，偶数的话需要记录第n/2个数然后求平均值
+     */
+    public static double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int length1 = nums1.length;
+        int length2 = nums2.length;
+        int n = length1 + length2;
+        //定义两个指针i,j
+        int i = 0, j = 0;
+        //right记录第n/2+1个数，left记录right前面一个数
+        int left = -1, right = -1;
+        //n/2+1次循环
+        for (int k = 0; k <= n / 2; k++) {
+            left = right;
+            //需要避免数组越界问题
+            if (i < length1 && (j >= length2 || nums1[i] <= nums2[j])) {
+                right = nums1[i];
+                i++;
+            } else {
+                right = nums2[j];
+                j++;
+            }
+        }
+        return n % 2 == 0 ? (left + right) / 2.0 : right;
+    }
+
+    /**
      * 合并数组，时间复杂度为O(m+n)
      */
     public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
