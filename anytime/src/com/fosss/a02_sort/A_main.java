0098e149c776b96242c2ac18c31aadfadd03fec5
fo <1745179058@qq.com>
2023-06-14 19:39:47 +0800

    anytime-a02排序算法-创建测试类

---
+++ anytime/src/com/fosss/a02_sort/A_main.java
@@ -0,0 +1,35 @@
+package com.fosss.a02_sort;
+
+/**
+ * @author: fosss
+ * Date: 2023/6/14
+ * Time: 19:35
+ * Description:测试类
+ */
+public class A_main {
+    public static final int MAXSIZE = 20;//要排序的顺序表的最大长度
+
+    public static void main(String[] args) {
+        int[] arr = new int[MAXSIZE + 1];//从下标1处开始存储数据，下标为0处用来做哨兵（相当于一个临时变量存储要插入的值）
+        //调用排序算法
+        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
