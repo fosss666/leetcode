fbaf1367ef5144185ebf565de3435bc03b6d3e60
fo <1745179058@qq.com>
2023-06-14 20:03:12 +0800

    anytime-a02排序算法-1.直接插入排序

--- anytime/src/com/fosss/a02_sort/A_main.java
+++ anytime/src/com/fosss/a02_sort/A_main.java
@@ -7,13 +7,37 @@ package com.fosss.a02_sort;
  * Description:测试类
  */
 public class A_main {
-    public static final int MAXSIZE = 20;//要排序的顺序表的最大长度
+    public static final int MAXSIZE = 8;//要排序的顺序表的最大长度
 
     public static void main(String[] args) {
         int[] arr = new int[MAXSIZE + 1];//从下标1处开始存储数据，下标为0处用来做哨兵（相当于一个临时变量存储要插入的值）
+        arr[1] = 13;
+        arr[2] = 6;
+        arr[3] = 3;
+        arr[4] = 31;
+        arr[5] = 9;
+        arr[6] = 27;
+        arr[7] = 5;
+        arr[8] = 11;
+        System.out.println("原数组：");
+        print(arr);
         //调用排序算法
-        
 
+        //1.直接插入排序
+        B01_InsertSort.insertSort(arr);
+        System.out.println("排序后的数组：");
+        print(arr);
+
+    }
+
+    /**
+     * 输出除哨兵外的数组的值
+     */
+    public static void print(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
     }
 }
 
---
+++ anytime/src/com/fosss/a02_sort/B01_InsertSort.java
@@ -0,0 +1,48 @@
+package com.fosss.a02_sort;
+
+/**
+ * @author: fosss
+ * Date: 2023/6/14
+ * Time: 19:43
+ * Description:直接插入排序
+ * 从第一个数开始，假定他为有序数组，从下一个数开始向这个有序数组中选择合适位置插入
+ */
+public class B01_InsertSort {
+    /**
+     * @param arr 要排序的数从下标1开始，下标0处为哨兵
+     */
+    public static void insertSort(int[] arr) {
+        //假定第一个数（下标为1处）已经有序，从下一处开始排序
+        for (int i = 2; i < arr.length; i++) {
+
+            //设置哨兵为要向有序数组中插入的数
+            arr[0] = arr[i];
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[0] < arr[j]) {
+                    //向后移动
+                    arr[j + 1] = arr[j];
+                } else {
+                    arr[j + 1] = arr[0];
+                    break;//退出循环
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
