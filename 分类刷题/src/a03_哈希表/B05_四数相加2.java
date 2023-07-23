3fd4166ce948809274b45d5bbbb7a16e5b533ed3
fo <1745179058@qq.com>
2023-07-23 21:38:23 +0800

    代码随想录-哈希表-四数相加2

---
+++ 分类刷题/src/a03_哈希表/B05_四数相加2.java
@@ -0,0 +1,57 @@
+package a03_哈希表;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: fosss
+ * Date: 2023/7/23
+ * Time: 20:19
+ * Description:
+ * 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
+ * 为了使问题简单化，所有的 A, B, C, D 具有相同的长度 N，且 0 ≤ N ≤ 500 。所有整数的范围在 -2^28 到 2^28 - 1 之间，最终结果不会超过 2^31 - 1 。
+ * 例如:
+ * 输入:
+ * A = [ 1, 2]
+ * B = [-2,-1]
+ * C = [-1, 2]
+ * D = [ 0, 2]
+ * 输出:2
+ * 解释:
+ * 两个元组如下:
+ * (0, 0, 0, 1) -> A[0] + B[0] + C[0] + D[1] = 1 + (-2) + (-1) + 2 = 0
+ * (1, 1, 0, 0) -> A[1] + B[1] + C[0] + D[0] = 2 + (-1) + (-1) + 0 = 0
+ */
+public class B05_四数相加2 {
+    /**
+     * 和两数相加思路差不多，区别在于分别将两个数组各元素的和作为一个元素放到map中
+     */
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            for (int j : nums2) {
+                map.put(i + j, map.getOrDefault(i + j, 0) + 1);
+            }
+        }
+
+        int res = 0;
+        for (int i : nums3) {
+            for (int j : nums4) {
+                res += map.getOrDefault(-(i + j), 0);
+            }
+        }
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
