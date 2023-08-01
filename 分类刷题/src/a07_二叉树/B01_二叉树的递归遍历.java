0720e6870c367c6c087ced17180bffc8e8ef8d5d
fo <1745179058@qq.com>
2023-08-01 14:07:34 +0800

    代码随想录-二叉树-二叉树的递归遍历

---
+++ 分类刷题/src/a07_二叉树/B01_二叉树的递归遍历.java
@@ -0,0 +1,85 @@
+package a07_二叉树;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: fosss
+ * Date: 2023/8/1
+ * Time: 14:01
+ * Description:
+ */
+public class B01_二叉树的递归遍历 {
+    /**
+     * 中序遍历
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        return list;
+    }
+
+    private void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+
+    /**
+     * 后序遍历
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        postorder(root, list);
+        return list;
+    }
+
+    private void postorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left, list);
+        postorder(root.right, list);
+        list.add(root.val);
+    }
+
+    /**
+     * 前序遍历
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        preorder(root, list);
+        return list;
+    }
+
+    private void preorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        list.add(root.val);
+        preorder(root.left, list);
+        preorder(root.right, list);
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
