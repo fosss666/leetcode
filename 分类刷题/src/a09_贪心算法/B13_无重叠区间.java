ccb68b1addaf848d1ae292bc2a6f5171da5e71aa
fosss <1745179058@qq.com>
2023-08-29 11:21:17 +0800

    贪心算法-无重叠区间

---
+++ 分类刷题/src/a09_贪心算法/B13_无重叠区间.java
@@ -0,0 +1,44 @@
+package a09_贪心算法;
+
+import java.util.Arrays;
+
+/**
+ * @author: fosss
+ * Date: 2023/8/29
+ * Time: 11:06
+ * Description:
+ * https://leetcode.cn/problems/non-overlapping-intervals/
+ * 给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
+ * 注意: 可以认为区间的终点总是大于它的起点。 区间 [1,2] 和 [2,3] 的边界相互“接触”，但没有相互重叠。
+ * 示例 1:
+ * 输入: [ [1,2], [2,3], [3,4], [1,3] ]
+ * 输出: 1
+ * 解释: 移除 [1,3] 后，剩下的区间没有重叠。
+ * 示例 2:
+ * 输入: [ [1,2], [1,2], [1,2] ]
+ * 输出: 2
+ * 解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+ * 示例 3:
+ * 输入: [ [1,2], [2,3] ]
+ * 输出: 0
+ * 解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+ */
+public class B13_无重叠区间 {
+
+    /**
+     * 与上一题相似，
+     */
+    public int eraseOverlapIntervals(int[][] intervals) {
+        //先按照左边界从小到大排序
+        Arrays.sort(intervals,(a, b)->Integer.compare(a[0],b[0]));
+        int res=0;
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<intervals[i-1][1]){
+                //i和i-1有重叠，去重叠数+1，取右边界小的那个
+                res++;
+                intervals[i][1]=Math.min(intervals[i][1],intervals[i-1][1]);
+            }
+        }
+        return res;
+    }
+}
